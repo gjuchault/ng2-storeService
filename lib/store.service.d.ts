f2226455c46d2d2872d6a553ee1cf891e2b363d5
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2016-07-15 10:30:47 +0200

    Add build version

--- lib/store.service.d.ts
+++ lib/store.service.d.ts
@@ -2,9 +2,11 @@ import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 export declare class StoreService {
     store: Store<any>;
-    storeModule: string;
-    path: Array<string>;
+    private storeModule;
+    private path;
+    private transform;
     constructor(store: Store<any>);
+    setTransformFunction(transformer: Function): void;
     retrieve(path: string): Observable<any>;
     dispatch(type: any, payload: Object): void;
 }
